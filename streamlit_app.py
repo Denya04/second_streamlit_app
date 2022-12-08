79b5cf2ac86a1e5c192dfa7f935a047342608da2
Denis Anishchanka <119801395+Denya04@users.noreply.github.com>
2022-12-08 21:50:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit
+import snowflake.connector
 
 txt_quote = "'"
 streamlit.title('Zina' + txt_quote + 's Amazing Athleisure Catalog')
+
+#Snowflake-related functions
+def get_catalog():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("SELECT * from catalog_for_website")
+       return my_cur.fetchall()
+      
+# Add a button to load catalog list
+if streamlit.button('Get Catalog'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_catalog()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)      
