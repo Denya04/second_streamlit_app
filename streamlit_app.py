343d749536b9d30795841fd3ec8acbd346621c3b
Denis Anishchanka <119801395+Denya04@users.noreply.github.com>
2022-12-08 22:08:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,11 @@ if streamlit.button('Get Catalog'):
   my_cnx.close()
   streamlit.dataframe(my_data_rows)      
 
+color_selection = my_data_rows.set_index('COLOR_OR_STYLE')
+
 option = streamlit.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+    'Pick a sweatsuit color or style:',
+     list(color_selection.index),[])
 
 #fruits_selected = streamlit.multiselect("Pick a sweatsuit color or style:", list(my_data_rows.index),[])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
