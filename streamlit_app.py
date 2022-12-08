72256989c1a5cf8694cf2b2998360e12b590c03a
Denis Anishchanka <119801395+Denya04@users.noreply.github.com>
2022-12-08 22:25:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,34 @@
 import streamlit
-import requests
+import pandas
 import snowflake.connector
 
 txt_quote = "'"
 streamlit.title('Zina' + txt_quote + 's Amazing Athleisure Catalog')
 
-#Snowflake-related functions
-def get_catalog():
-  with my_cnx.cursor() as my_cur:
-       my_cur.execute("SELECT * from catalog_for_website")
-       return my_cur.fetchall()
-      
-# Add a button to load catalog list
-#if streamlit.button('Get Catalog'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_catalog()
-  my_cnx.close()
-  streamlit.dataframe(my_data_rows)      
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+df = pandas.dataframe(my_catalog)
+
+streamlit.stop()
+my_cnx.close()
+streamlit.dataframe(my_data_rows)      
 
 color_selection = my_data_rows.set_index('COLOR_OR_STYLE')
-streamlit.stop()
+
+
 option = streamlit.selectbox(
     'Pick a sweatsuit color or style:',
      list(color_selection.index),[])
 
 #fruits_selected = streamlit.multiselect("Pick a sweatsuit color or style:", list(my_data_rows.index),[])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
+#Snowflake-related functions
+def get_catalog():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("SELECT * from catalog_for_website")
+       return my_cur.fetchall()
