8ee5d77001c0195ddfced1b66b04318c1b1920d2
Denis Anishchanka <119801395+Denya04@users.noreply.github.com>
2022-12-08 22:10:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ def get_catalog():
   my_cnx.close()
   streamlit.dataframe(my_data_rows)      
 
-color_selection = my_data_rows.set_index('COLOR_OR_STYLE')
+#color_selection = my_data_rows.set_index('COLOR_OR_STYLE')
 
-option = streamlit.selectbox(
+#option = streamlit.selectbox(
     'Pick a sweatsuit color or style:',
      list(color_selection.index),[])
 
