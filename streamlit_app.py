4f28ffb52aae1e410b26fe0dbc24ed5325a269cf
Denis Anishchanka <119801395+Denya04@users.noreply.github.com>
2022-12-08 22:44:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,11 @@ color_list = df[0].values.tolist()
 
 option = streamlit.selectbox(
     'Pick a sweatsuit color or style:',
-    color_list)
+    list(color_list))
 
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + option + "';")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
 df2[0],
